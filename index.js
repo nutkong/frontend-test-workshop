ec41f7297c5be66fd613b46fb198a80e48f11395
deathsy <kittipong.sri@mail.kmutt.ac.th>
2019-09-27 16:10:37 +0700

    :rotating_light: fix js linting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./src/App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './src/App'
 
-const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
+const root = document.getElementById('root')
+ReactDOM.render(<App />, root)
